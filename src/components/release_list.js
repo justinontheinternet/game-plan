ecde02d7bd76bfaa1ef9eaf6ce3d70ac1d60fb23
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-05-20 14:21:02 -0400

    filled in release dates

--- src/components/release_list.js
+++ src/components/release_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchReleaseList } from '../actions/index';
+import { Link } from 'react-router';
 
 class ReleaseList extends Component {
   componentWillMount() {
@@ -8,28 +9,42 @@ class ReleaseList extends Component {
   }
 
   renderReleaseList(game) {
-    console.log("game is", game);
+    let releaseDate = '';
+    console.log(game);
+
+    if (!game.original_release_date) {
+      releaseDate = game.expected_release_year + '-' +
+        game.expected_release_month + '-' +
+        game.expected_release_day;
+    } else {
+      releaseDate = game.original_release_date;
+    }
+
+
     return (
       <tr key={game.name}>
         <td>{game.name}</td>
-        <td>{game.original_release_date}</td>
+        <td>{releaseDate}</td>
       </tr>
     )
   }
 
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Release Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.releaseList.map(this.renderReleaseList)}
-        </tbody>
-      </table>
+      <div>
+        <h3 className="title">Upcoming Releases</h3>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Release Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.releaseList.map(this.renderReleaseList)}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
