6c0d5f2167278ddb9a4cc27ab79f16ccee2846d1
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-05-25 00:05:05 -0400

    various additions to GameShow component

--- src/components/game_show.js
+++ src/components/game_show.js
@@ -5,13 +5,42 @@ import { fetchGame } from '../actions/index';
 class GameShow extends Component {
   componentWillMount() {
     this.props.fetchGame(this.props.params.resourceId);
-    console.log("this.props is:", this.props);
+  }
+
+  listPlatforms(platform) {
+    return `\xa0\xa0\xa0${platform.name}`;
+  }
+
+  renderImages = (image) => {
+    return (
+      <img src={image.small_url} onClick={this.showScreenshot(image)} key={image.small_url} />
+    )
+  }
+
+  showScreenshot(image) {
+
   }
 
   render() {
-    console.log("this.props is 2:", this.props);
+    const { currentGame } = this.props;
+
+    if (!currentGame.id) {
+      return <h3>Loading...</h3>
+    }
+
+    console.log('in render', currentGame.images);
     return (
-      <p>games show</p>
+      <div>
+        <h1 className="title">{currentGame.name}</h1>
+        <p className="details pull-xs-right">
+          Platforms:<strong>{currentGame.platforms.map(this.listPlatforms)}</strong>
+        </p>
+        <p className="details">Developed by: <strong>{currentGame.developers[0].name}</strong></p>
+        <p className="details">Published by: <strong>{currentGame.publishers[0].name}</strong></p>
+        <div>
+          {currentGame.images.map(this.renderImages)}
+        </div>
+      </div>
     )
   }
 }
